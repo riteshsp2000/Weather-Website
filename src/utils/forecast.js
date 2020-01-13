4222fb886a63d22b3492c20f1652298ba403e823
Ritesh Patil <riteshp2000@Riteshs-MacBook-Pro.local>
2020-01-13 15:53:45 +0530

    Changed the data to be displayed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const moment = require('moment')
 
 const forecast = (latitude, longitude, callback) => {
   const url = 'https://api.darksky.net/forecast/2bdd1e851e5e1f286d7ec4d494be6f97/' + latitude + ',' + longitude + '?units=auto'
@@ -9,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (response.body.error) {
         callback('Unable to find location.', undefined)
     } else {
-        callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+        callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' °C out. There is a ' + response.body.currently.precipProbability + '% chance of rain. Sunrise: ' + moment(response.body.daily.data[0].sunriseTime, 'X').format('LT') + '. Sunset: ' + moment(response.body.daily.data[0].sunsetTime, "X").format('LT') + ' Maximum Temperature: ' + response.body.daily.data[0].temperatureHigh + '°C. Minimum Temperature: ' + response.body.daily.data[0].temperatureLow + '°C.')
     }
   })
 }
