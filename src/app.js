0df8ba16e3fa287afe12add414f7b066c53a2144
Ritesh Patil <riteshp2000@Riteshs-MacBook-Pro.local>
2020-01-13 17:04:46 +0530

    Made some major changes by myself

--- src/app.js
+++ src/app.js
@@ -15,9 +15,9 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 // Setup handlebars engine and views location
+hbs.registerPartials(partialsPath)
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
